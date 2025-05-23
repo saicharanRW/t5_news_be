8df46fa24a7e1d619709322217555e1bb16c602b
Sivaraman24122001 <162692641+Sivaraman24122001@users.noreply.github.com>
2025-05-23 12:21:56 +0530

    added cores and changes res format

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from news_apis_call import news
 from utils.process_data import process_catagory_data, process_location_data
 from utils.save_db import save_news, get_date
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 class NewsRequest(BaseModel):
     category: str
@@ -36,6 +44,5 @@ def get_news(payload: NewsRequest):
     #     #save_news(news_item)
     
     return {
-        "catagory" : processed_catagory,
-        "location" : processed_location
-    }       
+        "allnews" : processed_catagory + processed_location,
+    }
